8e26ac0c6b48b382119febfdc2c518dda649ac4c
ganggang <1326508285@qq.com>
2016-07-13 22:08:48 +0800

    tabCardw

---
+++ js/inc.js
@@ -0,0 +1,9 @@
+/*******************************
+*hiterbuy 公共js
+*author   zzlinux 菜鸟
+	*////////////////////////////
+var show=function(id){
+		var target=document.getElementById("id");
+		alert(target);
+//	alert('helloword')
+}
